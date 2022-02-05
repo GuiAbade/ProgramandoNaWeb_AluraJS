fd96d9498f9b6cb75338db6911637efa1e103132
Guilherme Abade <abade.guim13@gmail.com>
2022-02-04 22:36:00 -0300

    Animando remoção do paciente

--- introducao-javascript/js/remover-paciente.js
+++ introducao-javascript/js/remover-paciente.js
@@ -1,4 +1,7 @@
 var tabela = document.querySelector("#tabela-pacientes");
  tabela.addEventListener("click", function(event){
-   event.target.parentNode.remove();
+   event.target.parentNode.classList.add("fadeOut");
+      setTimeout(function(){
+        event.target.parentNode.remove();
+      }, 500);
  });
